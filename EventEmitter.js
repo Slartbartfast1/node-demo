23410c26e1654bd6fbd56440bc8ad9370e56843e
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-16 20:47:40 +0800

    订阅发布

---
+++ EventEmitter.js
@@ -0,0 +1,36 @@
+{
+    let events = require('events');
+
+    let Stream = new events.EventEmitter;
+
+    Stream.on('read', function (obj) {
+        console.log(obj + ' is on read')
+    });
+
+    Stream.emit('read', 'file1')
+}
+{
+    let events = require('events');
+    let util = require('util');
+
+    function Stream() {
+        events.EventEmitter.call(this)
+    }
+
+    util.inherits(Stream, events.EventEmitter);
+
+    Stream.prototype.write = function (file) {
+        this.emit("write", file);
+    };
+
+    var MyStream = new Stream();
+
+    MyStream.on("write", function (file) {
+        console.log(`${file} is writed`);
+    });
+    MyStream.write("file2");
+}
+
+
+
+
