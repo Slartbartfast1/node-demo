53321d2425937f29c83bf20ba137c445e96449ca
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-11-18 17:42:16 +0800

    Update limitTableMap.js

--- limitTableMap.js
+++ limitTableMap.js
@@ -1,25 +1,27 @@
-class limitTableMap{
-    constructor(limit){
-        this.limit=limit||100;
-        this.map={};
-        this.keys=[];
+class limitTableMap {
+    constructor(limit) {
+        this.limit = limit || 100;
+        this.map = {};
+        this.keys = [];
     }
-    set(key,value){
-        var map=this.map;
-        var keys=this.keys;
-        var hasOwnProperty=Object.prototype.hasOwnProperty();
-        if(!hasOwnProperty.call(map,key)){
-            if(keys.length===this.limit){
-                let firstKey=keys.shift;
+
+    set(key, value) {
+        var map = this.map;
+        var keys = this.keys;
+        var hasOwnProperty = Object.prototype.hasOwnProperty();
+        if (!hasOwnProperty.call(map, key)) {
+            if (keys.length === this.limit) {
+                let firstKey = keys.shift;
                 delete map[firstKey];
             }
             keys.push(key);
         }
-        map[key]=value;
+        map[key] = value;
     }
 
-    get(key){
+    get(key) {
         return this.map[key]
     }
 }
-module.exports=limitTableMap;
+
+module.exports = limitTableMap;
